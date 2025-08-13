9cfd8736bd35f0aa33c6dc011d097b749bb01524
libing.sun <762203640@qq.com>
2025-08-13 18:32:25 +0800

    update

--- routes/quotes.js
+++ routes/quotes.js
@@ -5,7 +5,10 @@ const quotes = require('../services/quotes');
 /* GET quotes listing. */
 router.get('/', async function(req, res, next) {
   try {
-    res.json(await quotes.getMultiple(req.query.page));
+    res.json({
+      code: 200,
+      ...await quotes.getMultiple(req.query.page)
+    })
   } catch (err) {
     console.error(`Error while getting quotes `, err.message);
     res.status(err.statusCode || 500).json({'message': err.message});
