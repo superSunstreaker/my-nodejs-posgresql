1a8b1dcf115254336c269594f4e03d0be06dd31b
libing.sun <762203640@qq.com>
2025-08-13 18:14:07 +0800

    update

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST || 'otto.db.elephantsql.com',
+    host: env.DB_HOST || '192.168.1.33',
     port: env.DB_PORT || '5432',
-    user: env.DB_USER || 'cklijfef',
-    password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
-    database: env.DB_NAME || 'cklijfef',
+    user: env.DB_USER || 'postgres',
+    password: env.DB_PASSWORD || 'postgres',
+    database: env.DB_NAME || 'my-pg1',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
